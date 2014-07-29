f8183dbd7dbfdb8e7d8a40ac01af0e94b25c0413
Zach Knickerbocker <zknicker@gmail.com>
2014-07-28 22:39:29 -0700

    Fun stuff - experimenting with display.

--- display.js
+++ display.js
@@ -1,11 +1,15 @@
+Display = {};
+
+Display.config = {
+}
 var meter = new FPSMeter($('.fps-meter')[0]);
 
 var pixelSize = 10;
 var pixelSpacing = 0;
 var pixelColor = '#1596CC';
 
-var screenHeight = innerHeight * 0.7; //px
-var screenWidth = innerWidth; //px
+var screenHeight = innerHeight * 0.7;
+var screenWidth = innerWidth;
 var xResolution = Math.ceil(screenWidth / (pixelSize + pixelSpacing));
 var yResolution = Math.floor(screenHeight / (pixelSize + pixelSpacing));
 
@@ -36,15 +40,50 @@ function animate() {
     meter.tickStart();
 
     randomizeAllPixels();
+    colorCenterPixel();
     context.putImageData(pixelDisplay, 0, 0);
 
     meter.tick();
     requestAnimFrame(animate);
 }
 
+function xyToDisplayPixel(x, y) {
+    return pixels[y * resolution.x + x];
+}
+
+var snakePixel = 0;
+var snakeMode = 0;
+var snakeThreshold = resolution.x;
+function snake() {
+    pixels[snakePixel].setColor(100, 200, 255, 255);
+    snakeThreshold--;
+    if (snakeThreshold <= 0) {
+      snakeThreshold = resolution.x;
+      snakePixel += resolution.x;
+      snakeMode = snakeMode == 1 ? 0 : 1;
+    }
+
+    if (snakeMode == 0) {
+        snakePixel++;
+        if (snakePixel >= pixels.length) {
+            snakePixel = 0;
+        }
+    } else {
+        snakePixel--;
+    }
+}
+
+function colorCenterPixel() {
+    var xCenter = Math.floor(resolution.x / 2);
+    var yCenter = Math.ceil((resolution.y - 4) / 2);
+
+    displayPixel = xyToDisplayPixel(xCenter, yCenter);
+    displayPixel.setColor(Math.random() * 255, Math.random() * 255, Math.random() * 255, 255);
+}
+
 function randomizeAllPixels() {
     for (var i = 0; i < pixels.length; i++) {
-        pixels[i].setColor(Math.random() * 255, Math.random() * 255, Math.random() * 255, 255);
+        pixels[i].setColor(Math.random() * 255, Math.random() * 255, Math.random() * 255, 50);
     }
 }
 
